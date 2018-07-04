2e1d6bbaaef2c375dc6b1c1f8d4bf19b00b387ac
johnsonng000 <ngks.johnson@gmail.com>
2018-07-04 19:48:59 +0800

    R Programming week 3 assignment

---
+++ ProgrammingAssignment2-master/cachematrix.R
@@ -0,0 +1,34 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+  invr <- NULL
+  set <- function(y){
+    x <<- y
+    invr <<- NULL
+  }
+  
+  get <- function() x
+  setInverse <- function(sInvr) invr <- sInvr
+  getInverse <- function() invr
+  list(set = set, get = get,
+       setInverse = setInverse, getInverse = getInverse)
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+    invr <- x$getInverse
+    if(!is.null(invr)){
+      message("getting cached data")
+      return(invr)
+    }
+    
+    data <- x$get()
+    invr <- solve(data, ...)
+    x$setInverse(invr)
+    invr
+}
